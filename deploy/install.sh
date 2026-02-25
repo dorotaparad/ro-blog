312c5410924507ce822af0fdb2974380f65fa610
Dorota Parad <git@dorotaparad.ch>
2026-02-25 14:57:59 +0100

    Upgrade Zola version, sync file permissions with repo

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 wget -q -O - \
-"https://github.com/getzola/zola/releases/download/v0.20.0/zola-v0.20.0-x86_64-unknown-linux-gnu.tar.gz" \
+"https://github.com/getzola/zola/releases/download/v0.22.1/zola-v0.22.1-x86_64-unknown-linux-gnu.tar.gz" \
 | tar xzf -
